e642f5011fd62b980bb32093880adb64ac91cc0a
Ethan <daniel.steensma@gmail.com>
2023-12-04 21:04:54 -0800

    headless = true

--- src/PlaywrightTests/UserInterfaceTests.cs
+++ src/PlaywrightTests/UserInterfaceTests.cs
@@ -21,7 +21,7 @@ public class UserInterfaceTests
         
         using IPlaywright playwright = await Playwright.CreateAsync();
         await using var browser =
-            await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions() { Headless = false, SlowMo = 50 });
+            await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions() { Headless = true, SlowMo = 50 });
 
         IBrowserContext context = await browser.NewContextAsync();
 
